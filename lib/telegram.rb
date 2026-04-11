b4892116aa44f2c04c2f09e0873a1b168d7674a2
Georgi Ganchev <googs.bs@gmail.com>
2026-04-11 09:55:12 +0100

    chore(tg): better entrypoint

---
+++ lib/telegram.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require "active_support/tagged_logging"
+
+require "telegram/client"
+require "telegram/poller"
+require "telegram/update_handler"
+
+module Telegram
+
+  def self.run
+    $stdout.sync = true
+
+    bot_token = ENV.fetch("TELEGRAM_BOT_TOKEN") do
+      raise "Missing TELEGRAM_BOT_TOKEN"
+    end
+
+    timeout = ENV.fetch("TELEGRAM_POLL_TIMEOUT", Poller::DEFAULT_TIMEOUT_SECONDS).to_i
+    error_backoff = ENV.fetch("TELEGRAM_POLL_ERROR_BACKOFF", Poller::DEFAULT_BACKOFF_SECONDS).to_i
+
+    logger = ActiveSupport::TaggedLogging.new(ActiveSupport::Logger.new($stdout))
+    logger.level = Rails.logger.level
+
+    client = Client.new(bot_token: bot_token, logger: logger)
+    handler = UpdateHandler.new(client: client, logger: logger)
+
+    Poller.new(
+      client: client,
+      handler: handler,
+      logger: logger,
+      timeout: timeout,
+      error_backoff: error_backoff
+    ).run
+  end
+
+end
